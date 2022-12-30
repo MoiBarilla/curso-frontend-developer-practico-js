c66c9b12d2c96b4ebb7813fb79dea7fe614a8a4c
MoiBarilla <moi.barilla@gmail.com>
2022-12-30 13:41:03 -0600

    added clase 13 to the html and css also created the corresponding functions

--- main.js
+++ main.js
@@ -4,11 +4,36 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
 navEmail.addEventListener('click', toggleDesktopMenu = () => {
+
+    let isProductDetailClosed = productDetail.classList.contains('inactive');
+    if (!isProductDetailClosed) {
+    productDetail.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 
 });
 
 menu.addEventListener('click', toggleHamburgerMenu = () => {
-    mobileMenu.classList.toggle('inactive');
+    let isProductDetailClosed = productDetail.classList.contains('inactive');
+    if(!isProductDetailClosed) {
+        productDetail.classList.add('inactive');
+    }
+        mobileMenu.classList.toggle('inactive');
+});
+
+shoppingCart.addEventListener('click', toggleShoppingCartProducts = () => {
+
+    let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    let isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if ( (!isMobileMenuClosed) || (!isDesktopMenuClosed) ){
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+        productDetail.classList.toggle('inactive');
+    
 });
